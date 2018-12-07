2de55b077f9e7be8eb208118c7b4200543acff84
Stefan Nesic <stefan.nesic@inria.fr>
2018-12-07 09:00:12 +0100

    Augmenting path search checks if node is in path to avoid cycles

--- ford_fulkerson.ml
+++ ford_fulkerson.ml
@@ -1,14 +1,15 @@
 open Graph
 
-(* ajouter check si noeud est déjà dans le chemin*)
 let chemin_augmentant gr s p = let rec trouve_chemin chemin arcs =
                                  match arcs with
                                  |[] -> []
-                                 |(id, x)::rest -> if id = p then (id, x)::chemin
-                                                   else
-                                                     let c = (trouve_chemin ((id, x)::chemin) (out_arcs gr id)) in
-                                                     if c = [] then trouve_chemin chemin rest 
-                                                     else c
+                                 |(id, x)::rest -> if List.mem (id,x) chemin then trouve_chemin chemin rest (* si noeud est déjà dans le chemin, pour éviter cycles *)
+                                                   else 
+                                                     (if id = p then (id, x)::chemin
+                                                      else
+                                                        let c = (trouve_chemin ((id, x)::chemin) (out_arcs gr id)) in
+                                                        if c = [] then trouve_chemin chemin rest 
+                                                        else c)
                                in List.rev (trouve_chemin [(s, max_int)] (out_arcs gr s));;   
 
 
